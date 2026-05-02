12610a79ad37da26e270bd867b4a3b61de86f585
soumalyaray <soumalyaray.work@gmail.com>
2026-05-03 00:58:24 +0530

    Add comment for report and header file import
    Added a comment to indicate the import of report and header file.

--- my-app/app/page.js
+++ my-app/app/page.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-
+{/* We have to import report and header file*/}
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
